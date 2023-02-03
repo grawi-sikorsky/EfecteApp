821a5b502c101c0e06a5ec7edf3ad8e6dc99654b
HomePC <grawires@gmail.com>
2023-02-03 18:33:46 +0100

    backend init

---
+++ EfecteBack/src/main/java/pl/js/efecteback/EfecteBackApplication.java
@@ -0,0 +1,13 @@
+package pl.js.efecteback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EfecteBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EfecteBackApplication.class, args);
+	}
+
+}
---
+++ EfecteBack/src/test/java/pl/js/efecteback/EfecteBackApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.js.efecteback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EfecteBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
